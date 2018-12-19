b60024a897537c33c578d26adfdc251f3ef7fe65
Sadavala, Bhanu Prakash <BHANU_PRAKASH_SADAVALA@homedepot.com>
2018-12-19 16:15:08 -0500

    xsd/xml parser

--- xmlparser/xmlparser.go
+++ xmlparser/xmlparser.go
@@ -6,6 +6,8 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+
+	"gopkg.in/go-playground/validator.v9"
 )
 
 type xmlresult struct {
@@ -14,52 +16,120 @@ type xmlresult struct {
 
 type orderCanonical struct {
 	//XMLName          xml.Name       `xml:"orderCanonical"`
-	TransactionCode  string         `xml:"transactionCode"`
-	CurrentTimestamp string         `xml:"currentTimestamp"`
-	OrderDetails     []orderDetails `xml:"orderDetails"`
+	TransactionCode  string       `xml:"transactionCode" validate:"required"`
+	CurrentTimestamp string       `xml:"currentTimestamp" validate:"required"`
+	OrderDetails     orderDetails `xml:"orderDetails" validate:"required"`
 }
 
 type orderDetails struct {
 	//XMLName     xml.Name      `xml:"orderDetails"`
-	OrderHeader []orderHeader `xml:"orderHeader"`
+	OrderHeader orderHeader `xml:"orderHeader" validate:"required"`
 }
 
 type orderHeader struct {
-	receivingLocationNumber       string
-	receivingLocationTypeCode     string
+	shippingLocationNumber        string
+	shippingLocationTypeCode      string
+	receivingLocationNumber       string `validate:"required"`
+	receivingLocationTypeCode     string `validate:"required"`
 	merchandisingVendorNumber     string
 	merchandisingDepartmentNumber string
 	distributionServiceTypeCode   string
 	replenishmentMethodCode       string
 	orderReasonCode               string
-	orderNumber                   string
+	orderNumber                   string `validate:"required"`
 	orderClass                    string
 	orderCreateDate               string
 	orderCreateTimestamp          string
 	lastUpdatedTimestamp          string
 	expectedArrivalDate           string
 	estimatedShipDate             string
+	cancelEligibilityDate         string
+	orderCancelTimestamp          string
+	orderClosedTimestamp          string
+	omtOrderTypeCode              string
+	accountOrderTypeCode          string
+	productOriginTypeCode         string
+	productDestinationTypeCode    string
+	orderRequestingSystemCode     string
+	peggedOrderFlag               string
+	specialOrderFlag              string
+	inboundOrderTypeCode          string
+	orderHeaderVersionNumber      string
+	orderStatusCode               string
+	orderOpenTimestamp            string
+	orderOpenSystemUserId         string
+	newStoreFlag                  string
+	consignmentFlag               string
+	commentText                   string
+	wmsShippingNumber             string
+	allocationId                  string
+	srcOrderRefId                 string
+	omtOrderRefTypeCode           string
+	vendorCertifiedFlag           string
+	orderTransmissionTypeCode     string
+	relatedOrders                 string
 }
 
+var validate *validator.Validate
+
+var v = orderCanonical{}
+
 func main() {
-	v := orderCanonical{TransactionCode: "test"}
+
+	validate = validator.New()
+
+	validateXML()
+}
+
+func validateXML() {
 
 	xmlfile, err := os.Open("sample.xml")
 
 	defer xmlfile.Close()
-	/*
-		scanner := bufio.NewScanner(xmlfile)
-		for scanner.Scan() {
-			myslice = myslice + scanner.Text()
-		} */
 
 	byteValue, _ := ioutil.ReadAll(xmlfile)
 
-	//fmt.Println(myslice)
 	err = xml.Unmarshal([]byte(byteValue), &v)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("%#v", v)
+	err = validate.Struct(v)
+	if err != nil {
+		if _, ok := err.(*validator.InvalidValidationError); ok {
+			fmt.Println(err)
+			return
+		}
+
+		for _, err := range err.(validator.ValidationErrors) {
+			fmt.Println(err.Namespace())
+			fmt.Println(err.Field())
+			fmt.Println(err.StructNamespace())
+			fmt.Println(err.StructField())
+			fmt.Println(err.Tag())
+			fmt.Println(err.ActualTag())
+			fmt.Println(err.Kind())
+			fmt.Println(err.Type())
+			fmt.Println(err.Value())
+			fmt.Println(err.Param())
+			fmt.Println()
+		}
+
+		return
+	}
+
+	//fmt.Printf("%#v", v)
+	copy()
+
+}
+
+func copy() {
+
+	copyxml, copyerr := xml.Marshal(v)
+	if copyerr != nil {
+		fmt.Println(copyerr)
+	}
+
+	fmt.Println(string(copyxml[:len(copyxml)]))
+
 }
--- xsdparser/xsdparser.go
+++ xsdparser/xsdparser.go
@@ -8,21 +8,49 @@ import (
 	"os"
 
 	samplexsd "github.com/bhanu/xsdparser/go_Sample"
+	"gopkg.in/go-playground/validator.v9"
+
+	//samplexsd "github.com/bhanu/xsdparser/go_order"
+	xsd "github.com/metaleap/go-xsd"
 )
 
+var validate *validator.Validate
+
 func main() {
-	/*
-		var (
-			sd  *xsd.Schema
-			err error
-		)
 
-		xsd.PkgGen.BaseCodePath = "./"
-		if sd, err = xsd.LoadSchema("sample.xsd", true); err != nil {
-			log.Fatal(err)
-		}
-	*/
+	validate = validator.New()
 
+	command := os.Args[1]
+
+	switch command {
+	case "g":
+		generateXSDTypes()
+		fmt.Println("Generating XSD types.")
+	case "v":
+		validateXML()
+		fmt.Println("Validating XSD.")
+	default:
+		fmt.Println("Inappropriate args.")
+		validateXML()
+	}
+
+}
+
+func generateXSDTypes() {
+	var (
+		sd  *xsd.Schema
+		err error
+	)
+
+	xsd.PkgGen.BaseCodePath = "./"
+	if _, err = xsd.LoadSchema("order.xsd", true); err != nil {
+		log.Fatal(err)
+	}
+
+	sd.MakeGoPkgSrcFile()
+}
+
+func validateXML() {
 	xmlfile, err := os.Open("sample.xml")
 	if err != nil {
 		log.Fatal(err)
@@ -36,12 +64,37 @@ func main() {
 
 	v.Walk()
 
-	fmt.Println(v.XsdGoPkgHasElem_OrderDetails.OrderDetails.OrderHeader.XsdGoPkgHasElem_OrderNumber.OrderNumber)
+	err = validate.Struct(v)
+	if err != nil {
+		if _, ok := err.(*validator.InvalidValidationError); ok {
+			fmt.Println(err)
+			return
+		}
+
+		for _, err := range err.(validator.ValidationErrors) {
+			fmt.Println(err.Namespace())
+			fmt.Println(err.Field())
+			fmt.Println(err.StructNamespace())
+			fmt.Println(err.StructField())
+			fmt.Println(err.Tag())
+			fmt.Println(err.ActualTag())
+			fmt.Println(err.Kind())
+			fmt.Println(err.Type())
+			fmt.Println(err.Value())
+			fmt.Println(err.Param())
+			fmt.Println()
+		}
+
+		return
+	}
+
+	//fmt.Println(v.XsdGoPkgHasElem_OrderDetails.OrderDetails.OrderHeader.XsdGoPkgHasElem_OrderNumber.OrderNumber)
+	//v.TransactionCode.Set("MODIFY")
+	//fmt.Println(v.TransactionCode)
 
 	if err != nil {
 		log.Fatalln(err)
 	}
 
 	fmt.Printf("%#v", v)
-
 }
