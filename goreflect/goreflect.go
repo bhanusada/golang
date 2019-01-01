9f52b56118867ce2ebbe68b61ed6306ee2cd2d30
Sadavala, Bhanu Prakash <BHANU_PRAKASH_SADAVALA@homedepot.com>
2018-12-31 21:05:52 -0500

    type check

--- goreflect/goreflect.go
+++ goreflect/goreflect.go
@@ -22,25 +22,45 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Println(xsd.TransactionCode)
+	//fmt.Println(xsd.TransactionCode)
 
+	/*
+
+		fmt.Println(t.Field(1))
+
+		f, found := t.FieldByName("RelatedOrderNumber")
+		if !found {
+			log.Print("Not found")
+		}
+		fmt.Println()
+
+		fmt.Println(f.Tag.Get("validate"))
+	*/
+	//xsd1 := map[string]interface{}(xsd)
+	filtertags(xsd.OrderDetails.OrderDetails)
+
+}
+
+func filtertags(xsd interface{}) {
+	fmt.Println(xsd)
 	t := reflect.TypeOf(xsd)
 
-	fmt.Println(t.Field(1))
-
-	f, found := t.FieldByName("RelatedOrderNumber")
-	if !found {
-		log.Print("Not found")
-	}
-	fmt.Println()
-
-	fmt.Println(f.Tag.Get("validate"))
-
-	//xsd1 := map[string]interface{}(xsd)
-
 	for i := 0; i < t.NumField(); i++ {
+		fmt.Println()
 		f1 := t.Field(i)
-		fmt.Println(f1.Type)
+		//fmt.Println(f1.Type)
+		fmt.Println(f1.Name)
+		switch reflect.TypeOf(f1).String() {
+		case "string":
+			fmt.Println("string")
+		case "int", "int16", "int32", "int64":
+			fmt.Println("int")
+		case "bool":
+			fmt.Println("bool")
+		default:
+			fmt.Println("Others")
+			filtertags(f1)
+		}
 	}
 
 }
