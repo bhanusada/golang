38769fc11b4d638ba9888267a341cf447085ed94
Sadavala, Bhanu Prakash <BHANU_PRAKASH_SADAVALA@homedepot.com>
2019-01-02 16:00:03 -0500

    slice working

--- goreflect/goreflect.go
+++ goreflect/goreflect.go
@@ -44,62 +44,63 @@ func main() {
 }
 
 func filtertags(xsd interface{}) {
-	//fmt.Println(xsd)
+
 	t := reflect.ValueOf(xsd)
-	//typeofT := t.Type()
-	if t.Kind() == reflect.Ptr && !t.IsNil() {
+
+	if t.Kind().String() == "ptr" && !t.IsNil() {
 		t = t.Elem()
 	}
-	//fmt.Println(t)
-	//fmt.Println(t.NumField())
+
 	for i := 0; i < t.NumField(); i++ {
-		//fmt.Println(i)
 		f1 := t.Field(i)
-		//fmt.Println(f1)
-		//fmt.Println(f1.String())
-		//fmt.Println(t.Field(i).Kind().String())
 		switch t.Field(i).Kind().String() {
 		case "string":
-			//fmt.Println("string")
 			if reflect.TypeOf(xsd).Field(i).Tag.Get("validate") == "required" {
-				fmt.Println(reflect.TypeOf(xsd).Field(i).Name)
+				fmt.Printf("Field %s is valid %t and value is %s\n", reflect.TypeOf(xsd).Field(i).Name, f1.IsValid(), f1)
+			}
+			field := reflect.ValueOf(xsd).FieldByName(reflect.TypeOf(xsd).Field(i).Name)
+			if !field.IsValid() {
+				fmt.Println("Does not exist ")
 			}
 		case "int", "int16", "int32", "int64":
-			//fmt.Println("int")
 			if reflect.TypeOf(xsd).Field(i).Tag.Get("validate") == "required" {
 				fmt.Println(reflect.TypeOf(xsd).Field(i).Name)
 			}
 		case "float32", "float64":
-			//fmt.Println("float")
 			if reflect.TypeOf(xsd).Field(i).Tag.Get("validate") == "required" {
 				fmt.Println(reflect.TypeOf(xsd).Field(i).Name)
 			}
 		case "bool":
-			//fmt.Println("bool")
 			if reflect.TypeOf(xsd).Field(i).Tag.Get("validate") == "required" {
 				fmt.Println(reflect.TypeOf(xsd).Field(i).Name)
 			}
 		case "slice":
-			fmt.Println("slice")
+			if reflect.TypeOf(xsd).Field(i).Tag.Get("validate") == "required" {
+				fmt.Println(reflect.TypeOf(xsd).Field(i).Name)
+			}
 			slicestruct(f1.Interface())
 		case "struct":
+			if reflect.TypeOf(xsd).Field(i).Tag.Get("validate") == "required" {
+				fmt.Println(reflect.TypeOf(xsd).Field(i).Name)
+			}
 			filtertags(f1.Interface())
 		default:
 			fmt.Println("Others")
-			//fmt.Println(f1.Kind().String())
-			//filtertags(reflect.ValueOf(f1))
 		}
 	}
 
 }
 
 func slicestruct(v interface{}) {
+
 	s := reflect.ValueOf(v)
 
 	for i := 0; i < s.Len(); i++ {
-		if reflect.TypeOf(s.Index(i)).Kind() == reflect.Struct {
-			filtertags(s.Index(i))
+		switch s.Index(i).Kind().String() {
+		case "struct":
+			filtertags(s.Index(i).Interface())
+		case "slice":
+			slicestruct(s.Index(i).Interface())
 		}
-		//fmt.Println(s.Index(i))
 	}
 }
